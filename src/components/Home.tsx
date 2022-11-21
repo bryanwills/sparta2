07897588653d77c51d6a6176205bbaa1aa2b713f
Bryan Wills <bryanwi09@gmail.com>
2022-11-20 20:11:09 -0500

    added alt tag to img component on Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
       <div className="w-screen">
           <div className="px-4 py-4 mx-auto lg:max-w-7xl items-center md:flex md:px-8 place-content-center relative">
-             <img src={image} className="hover:bg-blend-darken brightness-50 object-cover" />
+             <img src={image} className="hover:bg-blend-darken brightness-50 object-cover" alt=""/>
              <button
                className=
                 "absolute xl:bottom-20 xl:left-40 bottom-20 left-20 bg-[#e3b151] text-[#080e18] px-2 py-2 rounded-2xl hover:bg-[#bf1e2e] sm:width-50">
